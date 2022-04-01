9005fffbe0fe3e2c4553647386e964341badc659
Smael Nicolas <nico.smael@gmail.com>
2022-04-01 17:45:17 -0300

    add sample user

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -14,53 +14,67 @@ import { Users } from 'src/app/class/users/users';
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-  userList: Users[] = [];
+  userList: Users[] = [
+    {
+      id: 1,
+      name: 'Nicolás',
+      lastName: 'Smael',
+      username: 'userNico',
+      email: 'nico.smael@gmail.com',
+    },
+  ];
   userForm!: FormGroup;
+
   constructor(public uf: FormBuilder) {
     this.userForm = this.uf.group({
       name: new FormControl('', [
-        Validators.minLength(3),
         Validators.required,
-        Validators.maxLength(15),
-        Validators.pattern('[a-zA-Z ]*'),
-      ]),
-      lastName: new FormControl('', [
-        Validators.required,
+        this.noWhitespaceValidator,
         Validators.minLength(3),
         Validators.maxLength(15),
         Validators.pattern('[a-zA-Z ]*'),
       ]),
+      lastName: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          this.noWhitespaceValidator,
+          Validators.minLength(3),
+          Validators.maxLength(15),
+          Validators.pattern('[a-zA-Z ]*'),
+        ])
+      ),
       username: new FormControl('', [
         Validators.required,
+        this.noWhitespaceValidator,
         Validators.minLength(3),
         Validators.maxLength(15),
       ]),
       password: new FormControl('', [
         Validators.required,
-        Validators.minLength(3),
+        this.noWhitespaceValidator,
+        Validators.minLength(5),
         Validators.maxLength(15),
       ]),
       email: new FormControl('', [
         Validators.required,
+        this.noWhitespaceValidator,
         Validators.email,
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
-      ]),
-      address: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(15),
-      ]),
-      website: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(15),
       ]),
     });
   }
 
   ngOnInit(): void {}
 
+  public noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { whitespace: true };
+  }
+
   saveUser() {
     this.userList.push(this.userForm.value);
+    this.userForm.reset();
   }
 }
